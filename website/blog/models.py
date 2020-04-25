60f163471e31f3c9ec92d248145adb34256b699d
liwengang <lwgzx0706@126.com>
2020-04-25 23:32:23 +0800

    1

--- website/blog/models.py
+++ website/blog/models.py
@@ -3,24 +3,24 @@ from django.db import models
 
 
 class Category(models.Model):
-    category_name = models.CharField(default="无", max_length=20)
-    category_info = models.CharField(default="无", max_length=100)
+    category_name = models.CharField(default="", max_length=20)
+    category_info = models.CharField(default="", max_length=100)
 
 
 class Tag(models.Model):
-    tag_name = models.CharField(default="无", max_length=20)
-    tag_info = models.CharField(default="无", max_length=100)
+    tag_name = models.CharField(default="", max_length=20)
+    tag_info = models.CharField(default="", max_length=100)
 
 
 class Entry(models.Model):
-    title = models.CharField(default="无", max_length=128)
-    author = models.CharField(default="无", max_length=10)
+    title = models.CharField(default="", max_length=128)
+    author = models.CharField(default="", max_length=10)
     img = models.ImageField(upload_to='blog_img', null=True, blank=True, verbose_name='博客配图')
-    body = models.TextField(default="无")
-    abstract = models.TextField(default="无", max_length=256, null=True, blank=True)
+    body = models.TextField(default="")
+    abstract = models.TextField(default="", max_length=256, null=True, blank=True)
     visiting = models.PositiveIntegerField(default=0)
-    category = models.CharField(default="无", max_length=20)
-    tags = models.CharField(default="无", max_length=20)
+    category = models.CharField(default="", max_length=20)
+    tags = models.CharField(default="", max_length=20)
     created_time = models.DateTimeField('创建时间', auto_now_add=True)
     modified_time = models.DateTimeField('修改时间', auto_now=True)
 
