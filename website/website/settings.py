b6b3fbdc5f92c1c82359513f520517087017153c
liwengang <lwgzx0706@126.com>
2020-05-14 22:35:48 +0800

    0.0

--- website/website/settings.py
+++ website/website/settings.py
@@ -121,7 +121,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'blog/static'),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace("//", "/")
